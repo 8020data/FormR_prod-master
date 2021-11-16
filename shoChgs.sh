5b99ba2f44a2aa5ff3a53fd08afd21e8d98c461d
robinmattern <robin.mattern@gmail.com>
2021-11-16 10:26:36 -0500

    Fixed Update for 3c-rauth

--- shoChgs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# grep -rn ".($1" *
- grep -rn --exclude-dir={node_modules,.git} ".($1" * | awk '{ n = index($0,":"); a = substr($0,n+1); p = index(a,":"); printf "%70s %4d %s\n", substr($0,1,n-1), substr(a,1,p-1), substr(a,p+1) }'
-
